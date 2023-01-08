7ee0a5774bd8007f9954ecf1b49f2bbcacf318c7
bbudusan <bbudusan@yahoo.com>
2023-01-08 21:04:02 +0200

    Add project files.

---
+++ Budusan_Bogdan-Cristian_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Budusan_Bogdan_Cristian_Lab11.Data;
+namespace Budusan_Bogdan_Cristian_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
---
+++ Budusan_Bogdan-Cristian_Lab11/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace Budusan_Bogdan_Cristian_Lab11
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
